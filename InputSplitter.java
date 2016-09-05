4d100f11a6f8c41f5774600a197935d5d28d11ec
John <track394@users.noreply.github.com>
2016-09-05 12:27:24 -0700

    Add files via upload

---
+++ InputSplitter.java
@@ -0,0 +1,43 @@
+/*
+ * Group members: John Hardy, Albert Didde, and Ryan Frizel.
+ */
+
+package testing;
+
+import java.util.Scanner;
+
+public class InputSplitter {
+	
+	public static void main(String [] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Give me any sentence and I will tell you how many individual words it has.");
+		String sentence = input.nextLine();
+		String[] words = sentence.split(" ");
+		for (int i = 0; i != words.length; i++) {
+			boolean shouldSetToNull = false;
+			for (int j = 0; j != words.length; j++) {
+				if (words[j] == null || words[i] == null || j == i) {
+					continue;
+				} else if (words[i].equals(words[j])) {
+					shouldSetToNull = true;//here is your flag prof. Forney
+					words[j] = null;
+				}
+			}
+			if (shouldSetToNull) {
+				words[i] = null;
+			}
+		}
+		boolean foundIndividual = false;
+		for (int i = 0; i != words.length; i++) {
+			if (words[i] != null) {
+				System.out.println(words[i]);
+				foundIndividual = true;
+			}
+		}
+		if (foundIndividual) {
+			System.out.println("The above words are individual.");
+		} else {
+			System.out.println("No individual words found...");
+		}
+	}
+}
